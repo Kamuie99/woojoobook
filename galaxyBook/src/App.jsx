aef1c184317abfb2597c6684e9a56b17946c6361
dakgoo02 <dakgoo02@naver.com>
2024-07-17 17:56:54 +0900

    [#1] feat: 기본 레이아웃 설정 및 폰트 설정

--- galaxyBook/src/App.jsx
+++ galaxyBook/src/App.jsx
@@ -1,10 +1,27 @@
 import './App.css'
+import { Routes, Route, Link } from 'react-router-dom';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Notfound from './pages/Notfound';
+
+// 1. "/": home 페이지
+// 2. "/login": login 페이지
+
 
 function App() {
   return (
-    <div>
-      hello
-    </div>
+    <>
+      <div>
+        <Link to={"/"}>홈으로</Link>
+        <Link to={"/login"}>로그인</Link>
+      </div>
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/login" element={<Login />} />
+        <Route path="*" element={<Notfound />} />
+      </Routes>
+    
+    </>
   )
 }
 
