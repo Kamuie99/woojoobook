c4e4206eebbd97acecb5bcc1c3e7901d9672359b
kwondh5217 <trewq231@naver.com>
2024-07-17 12:36:04 +0900

    feat: 회원 엔티티 구현

---
+++ src/main/java/com/e207/woojoobook/domain/user/User.java
@@ -0,0 +1,31 @@
+package com.e207.woojoobook.domain.user;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "users")
+@Entity
+public class User {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String email;
+	private String password;
+	private String nickname;
+	private String areaCode;
+
+	@Builder
+	private User(String email, String password, String nickname, String areaCode) {
+		this.email = email;
+		this.password = password;
+		this.nickname = nickname;
+		this.areaCode = areaCode;
+	}
+}
