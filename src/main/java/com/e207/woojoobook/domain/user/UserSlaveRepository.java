0605f14c2e2406fd62ec85d76ebfc382d16c9e80
kwondh5217 <trewq231@naver.com>
2024-07-17 17:46:09 +0900

    feat: email 중복 체크

--- src/main/java/com/e207/woojoobook/domain/user/UserSlaveRepository.java
+++ src/main/java/com/e207/woojoobook/domain/user/UserSlaveRepository.java
@@ -3,4 +3,5 @@ package com.e207.woojoobook.domain.user;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserSlaveRepository extends JpaRepository<User, Long> {
+	boolean existsByEmail(String email);
 }
