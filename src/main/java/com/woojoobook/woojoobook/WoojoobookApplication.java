cbad8c9e1367a2aadbf0de61b0d95dc95f6054bf
jhl221123 <jhl221123@naver.com>
2024-07-17 10:47:53 +0900

    [#1] chore: 프로젝트 생성

---
+++ src/main/java/com/woojoobook/woojoobook/WoojoobookApplication.java
@@ -0,0 +1,13 @@
+package com.woojoobook.woojoobook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WoojoobookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WoojoobookApplication.class, args);
+	}
+
+}
