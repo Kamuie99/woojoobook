63a1167775c65d08eac6f3a9c3eec05db4a466cc
kwondh5217 <trewq231@naver.com>
2024-07-17 17:05:09 +0900

    feat: 회원가입, 메일 인증 기능 구현

--- src/main/java/com/e207/woojoobook/api/user/UserService.java
+++ src/main/java/com/e207/woojoobook/api/user/UserService.java
@@ -1,7 +1,13 @@
 package com.e207.woojoobook.api.user;
 
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.e207.woojoobook.api.mail.MailSender;
+import com.e207.woojoobook.api.mail.VerificationMail;
+import com.e207.woojoobook.domain.user.UserMasterRepository;
+import com.e207.woojoobook.domain.user.UserSlaveRepository;
 import com.e207.woojoobook.domain.user.UserVerification;
 import com.e207.woojoobook.domain.user.UserVerificationRepository;
 import com.e207.woojoobook.domain.user.VerificationCodeUtil;
@@ -13,37 +19,55 @@ import lombok.RequiredArgsConstructor;
 public class UserService {
 
 	private final UserVerificationRepository userVerificationRepository;
+	private final UserSlaveRepository userSlaveRepository;
 	private final UserMasterRepository userMasterRepository;
+	private final PasswordEncoder passwordEncoder;
+	private final MailSender mailSender;
 
+	@Transactional
 	public void join(UserCreateRequest userCreateRequest) {
 		UserVerification userVerification = this.userVerificationRepository.findById(userCreateRequest.getEmail())
-			.orElseThrow(() -> new RuntimeException("존재하지 않는 email"));
-		if(!userVerification.isVerified()) {
-			throw new RuntimeException("인증되지 않은 회원");
+			.orElseThrow(() -> new RuntimeException("유효하지 않는 email입니다."));
+		if (!userVerification.isVerified()) {
+			throw new RuntimeException("인증되지 않은 회원입니다.");
 		}
 
+		userCreateRequest.encode(this.passwordEncoder.encode(userCreateRequest.getPassword()));
 
+		this.userMasterRepository.save(userCreateRequest.toEntity());
 	}
 
-
-
-	public boolean verifyMember(String email, String code) {
-		UserVerification userVerification = this.userVerificationRepository.findById(email)
+	@Transactional
+	public boolean verifyEmail(VerificationMail verificationMail) {
+		UserVerification userVerification = this.userVerificationRepository.findById(verificationMail.getEmail())
 			.orElseThrow(() -> new RuntimeException("존재하지 않는 email"));
-		userVerification.verify(code);
+		userVerification.verify(verificationMail.getVerificationCode());
 
 		UserVerification save = this.userVerificationRepository.save(userVerification);
 
 		return save.isVerified();
 	}
 
-	public void createVerificationMember(String email) {
+	@Transactional
+	public void sendMail(EmailCodeCreateRequest request) {
+		String email = request.getEmail();
+		String verificationCode = createVerificationUser(email);
+
+		VerificationMail verificationMail = VerificationMail.builder()
+			.email(email)
+			.verificationCode(verificationCode)
+			.build();
+
+		this.mailSender.send(verificationMail);
+	}
+
+	private String createVerificationUser(String email) {
 		UserVerification userVerification = UserVerification.builder()
 			.email(email)
 			.verificationCode(VerificationCodeUtil.generate())
 			.isVerified(false)
 			.build();
 
-		this.userVerificationRepository.save(userVerification);
+		return this.userVerificationRepository.save(userVerification).getVerificationCode();
 	}
 }
