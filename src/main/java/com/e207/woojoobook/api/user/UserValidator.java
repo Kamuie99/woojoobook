72d8fda0d23b34c4a3a710bf5ef13ca3439e6bce
kwondh5217 <trewq231@naver.com>
2024-07-17 17:04:39 +0900

    feat: 회원가입 dto validator 구현

---
+++ src/main/java/com/e207/woojoobook/api/user/UserValidator.java
@@ -0,0 +1,23 @@
+package com.e207.woojoobook.api.user;
+
+import org.springframework.stereotype.Component;
+import org.springframework.validation.Errors;
+import org.springframework.validation.Validator;
+
+@Component
+public class UserValidator implements Validator {
+
+	@Override
+	public boolean supports(Class<?> clazz) {
+		return UserCreateRequest.class.isAssignableFrom(clazz);
+	}
+
+	@Override
+	public void validate(Object target, Errors errors) {
+		UserCreateRequest request = (UserCreateRequest)target;
+
+		if (!request.getPassword().equals(request.getPasswordConfirm())) {
+			errors.rejectValue("passwordConfirm", "비밀번호가 일치하지 않습니다.");
+		}
+	}
+}
