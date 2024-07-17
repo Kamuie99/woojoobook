7fb0f16451cafe7f8507ae56d8dac60262dd4a8c
kwondh5217 <trewq231@naver.com>
2024-07-17 17:05:30 +0900

    test: 회원가입, 메일 인증 컨트롤러 단위 테스트

---
+++ src/test/java/com/e207/woojoobook/api/user/UserControllerTest.java
@@ -0,0 +1,120 @@
+package com.e207.woojoobook.api.user;
+
+import static org.mockito.BDDMockito.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import com.e207.woojoobook.api.mail.VerificationMail;
+import com.e207.woojoobook.global.security.SecurityConfig;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+@Import({SecurityConfig.class, UserValidator.class})
+@WebMvcTest(UserController.class)
+class UserControllerTest {
+
+	@MockBean
+	private UserService userService;
+	@Autowired
+	private MockMvc mockMvc;
+	@Autowired
+	private ObjectMapper objectMapper;
+
+	@DisplayName("인증이 완료된 회원이 회원가입을 한다")
+	@Test
+	void createMember_success() throws Exception {
+		// given
+		String email = "test@test.com";
+		String password = "password";
+		String passwordConfirm = "password";
+		String nickname = "nickname";
+		UserCreateRequest userCreateRequest = createUserCreateRequest(email, password, passwordConfirm, nickname);
+
+		// when
+		ResultActions resultActions = this.mockMvc.perform(MockMvcRequestBuilders.post("/users")
+			.contentType(MediaType.APPLICATION_JSON)
+			.content(this.objectMapper.writeValueAsString(userCreateRequest)));
+
+		// then
+		resultActions.andExpect(status().isCreated())
+			.andDo(print());
+	}
+
+	@DisplayName("비밀번호가 일치하지 않다면 회원가입은 실패한다")
+	@Test
+	void createMember_fail_password() throws Exception {
+		// given
+		String email = "test@test.com";
+		String password = "notSamePassword";
+		String passwordConfirm = "password";
+		String nickname = "nickname";
+		UserCreateRequest userCreateRequest = createUserCreateRequest(email, password, passwordConfirm, nickname);
+
+		// when
+		ResultActions resultActions = this.mockMvc.perform(MockMvcRequestBuilders.post("/users")
+			.contentType(MediaType.APPLICATION_JSON)
+			.content(this.objectMapper.writeValueAsString(userCreateRequest)));
+
+		// then
+		resultActions.andExpect(status().is4xxClientError())
+			.andDo(print());
+	}
+
+	@DisplayName("이메일 인증코드를 발송한다")
+	@Test
+	void sendVerificationCode_success() throws Exception {
+		// given
+		String email = "test@test.com";
+		EmailCodeCreateRequest request = new EmailCodeCreateRequest();
+		request.setEmail(email);
+
+		// when
+		ResultActions resultActions = this.mockMvc.perform(MockMvcRequestBuilders.post("/users/emails")
+			.contentType(MediaType.APPLICATION_JSON)
+			.content(this.objectMapper.writeValueAsString(request)));
+
+		// then
+		resultActions.andExpect(status().isCreated());
+	}
+
+	@DisplayName("정확한 인증 코드라면 이메일 인증을 완료한다")
+	@Test
+	void verifyEmail_success() throws Exception {
+		// given
+		VerificationMail verificationMail = VerificationMail.builder()
+			.email("test@test.com")
+			.verificationCode("validToken")
+			.build();
+
+		given(this.userService.verifyEmail(any(VerificationMail.class))).willReturn(true);
+
+		// when
+		ResultActions resultActions = this.mockMvc.perform(MockMvcRequestBuilders.put("/users/emails")
+			.contentType(MediaType.APPLICATION_JSON)
+			.content(objectMapper.writeValueAsString(verificationMail)));
+
+		// then
+		resultActions.andExpect(status().isOk());
+	}
+
+	private static UserCreateRequest createUserCreateRequest(String email, String password, String passwordConfirm,
+		String nickname) {
+		return UserCreateRequest.builder()
+			.email(email)
+			.password(password)
+			.passwordConfirm(passwordConfirm)
+			.nickname(nickname)
+			.areaCode("areaCode")
+			.build();
+	}
+}
