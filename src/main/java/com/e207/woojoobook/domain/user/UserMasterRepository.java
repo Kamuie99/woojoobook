4eaf0ab86b7ec7f0edd4aa0c2ec78a4b3f26e6d2
kwondh5217 <trewq231@naver.com>
2024-07-17 17:06:21 +0900

    feat: 회원 리포지토리 구현

--- src/main/java/com/e207/woojoobook/domain/user/UserMasterRepository.java
+++ src/main/java/com/e207/woojoobook/domain/user/UserMasterRepository.java
@@ -1,8 +1,6 @@
-package com.e207.woojoobook.api.user;
+package com.e207.woojoobook.domain.user;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.e207.woojoobook.domain.user.User;
 
 public interface UserMasterRepository extends JpaRepository<User, Long> {
 }
