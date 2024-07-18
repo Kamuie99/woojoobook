65e121bc9ef56e3f546858e1bee5940f517e4ff6
kwondh5217 <trewq231@naver.com>
2024-07-18 09:03:57 +0900

    chore: TODO 주석 추가

--- src/main/java/com/e207/woojoobook/global/security/jwt/KeyGenerator.java
+++ src/main/java/com/e207/woojoobook/global/security/jwt/KeyGenerator.java
@@ -4,6 +4,7 @@ import java.security.SecureRandom;
 import java.util.Base64;
 
 // 랜덤 시크릿 키를 생성하는 클래스
+// TODO : 외부에서 관리되어야 함
 public class KeyGenerator {
     public static void main(String[] args) {
         SecureRandom random = new SecureRandom();
