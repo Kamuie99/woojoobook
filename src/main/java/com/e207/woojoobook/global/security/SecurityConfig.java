af0b8fd271687f2034b040df2a52b43cfafc9abe
최지우 <156387971+ji-ooo@users.noreply.github.com>
2024-07-19 16:27:42 +0900

    [#20] fix: cors 추가 (#21)

--- src/main/java/com/e207/woojoobook/global/security/SecurityConfig.java
+++ src/main/java/com/e207/woojoobook/global/security/SecurityConfig.java
@@ -1,5 +1,7 @@
 package com.e207.woojoobook.global.security;
 
+import java.util.Arrays;
+
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -14,6 +16,9 @@ import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import com.e207.woojoobook.domain.user.UserSlaveRepository;
 import com.e207.woojoobook.global.security.jwt.JwtAccessDeniedHandler;
@@ -68,6 +73,9 @@ public class SecurityConfig {
 
 		return http
 			.httpBasic(AbstractHttpConfigurer::disable)
+			.cors(customizer ->
+				customizer
+					.configurationSource(corsConfigurationSource()))
 			.csrf(AbstractHttpConfigurer::disable)
 			.headers(headers ->
 				headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::disable))
@@ -91,5 +99,18 @@ public class SecurityConfig {
 	@Bean
 	PasswordEncoder passwordEncoder() {
 		return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+	}
+
+	@Bean
+	CorsConfigurationSource corsConfigurationSource() {
+		CorsConfiguration configuration = new CorsConfiguration();
+		configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173"));
+		configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PATCH", "PUT", "DELETE", "OPTIONS"));
+		configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
+		configuration.setExposedHeaders(Arrays.asList("Authorization"));
+		configuration.setAllowCredentials(true);
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", configuration);
+		return source;
 	}
 }
