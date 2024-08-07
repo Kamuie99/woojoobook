83767fcbdf95649d67e6c79494634a3e91259257
kwondh5217 <trewq231@naver.com>
2024-08-07 10:28:19 +0900

    [#121] fix: 레디스 직렬화, 역직렬화 설정 수정

--- src/main/java/com/e207/woojoobook/global/redis/RedisConfig.java
+++ src/main/java/com/e207/woojoobook/global/redis/RedisConfig.java
@@ -7,8 +7,6 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
-import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
-import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Profile("!test")
 @Configuration
@@ -29,14 +27,6 @@ public class RedisConfig {
 	public RedisTemplate<?, ?> redisTemplate(LettuceConnectionFactory connectionFactory) {
 		RedisTemplate<byte[], byte[]> template = new RedisTemplate<>();
 		template.setConnectionFactory(connectionFactory);
-		GenericJackson2JsonRedisSerializer jsonSerializer = new GenericJackson2JsonRedisSerializer();
-
-		template.setKeySerializer(new StringRedisSerializer());
-		template.setValueSerializer(jsonSerializer);
-		template.setHashKeySerializer(new StringRedisSerializer());
-		template.setHashValueSerializer(jsonSerializer);
-
-		template.setDefaultSerializer(jsonSerializer);
 		template.afterPropertiesSet();
 		return template;
 	}
