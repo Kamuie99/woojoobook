67827d3042c195988cfaf8b87ae6bc5ce21a8807
kwondh5217 <trewq231@naver.com>
2024-08-07 11:08:59 +0900

    [#123] fix: 레디스 에러 수정

--- src/main/java/com/e207/woojoobook/api/chat/response/UserOnResponse.java
+++ src/main/java/com/e207/woojoobook/api/chat/response/UserOnResponse.java
@@ -1,9 +1,11 @@
 package com.e207.woojoobook.api.chat.response;
 
 
+import java.io.Serializable;
+
 import com.e207.woojoobook.domain.user.User;
 
-public record UserOnResponse (Long id, String nickname){
+public record UserOnResponse (Long id, String nickname) implements Serializable {
     public static UserOnResponse toDto(User user) {
         return new UserOnResponse(user.getId(), user.getNickname());
     }
